2bae1c46f0e6d4487c7f8cf45fa57bfdcc0d02fa
paas <paas@cse-codegen-backend-3882710532-kzs0n>
2018-08-14 14:52:51 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/myapp/controller/MyappImpl.java
@@ -0,0 +1,28 @@
+package com.service.myapp.controller;
+
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringDemoCodegen", date = "2018-08-14T14:52:51.107Z")
+
+@RestSchema(schemaId = "myapp")
+@RequestMapping(path = "/myapp", produces = MediaType.APPLICATION_JSON)
+public class MyappImpl {
+
+    @Autowired
+    private MyappDelegate userMyappDelegate;
+
+
+    @RequestMapping(value = "/helloworld",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String helloworld( @RequestParam(value = "name", required = true) String name){
+
+        return userMyappDelegate.helloworld(name);
+    }
+
+}
